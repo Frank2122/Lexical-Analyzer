5997fd0229c7efa2e66b2cca6814ffe0ee4abeec
Frank9661 <98559684+Frank9661@users.noreply.github.com>
2023-11-08 18:12:59 -0800

    Updated tokenize function
    Function now ignores phrases after "//" in the same line. So adding code below it will not be affected and it still works

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ SEPARATORS = ["(", ")", "{", "}", "[", "]", ";", ","]
 
 def tokenize(code):
     tokens = []
-    for match in re.finditer(r"(\b\w+\b)|(\W)", code):
-        match = match.group(0)
-        if match in KEYWORDS:
-            tokens.append((match, "KEYWORD"))
-        elif match in OPERATORS:
-            tokens.append((match, "OPERATOR"))
-        elif match in SEPARATORS:
-            tokens.append((match, "SEPARATOR"))
-        elif match.isdigit():
-            tokens.append((match, "INTEGER"))
-        elif match.isalpha():
-            tokens.append((match, "IDENTIFIER"))
+    lines = code.split('\n')
+    for line in lines:
+        # Remove comments in each line
+        line = line.split('//')[0]
+        for match in re.finditer(r"(\b\w+\b)|(\W)", line):
+            match = match.group(0)
+            if match in KEYWORDS:
+                tokens.append((match, "KEYWORD"))
+            elif match in OPERATORS:
+                tokens.append((match, "OPERATOR"))
+            elif match in SEPARATORS:
+                tokens.append((match, "SEPARATOR"))
+            elif match.isdigit():
+                tokens.append((match, "INTEGER"))
+            elif match.isalpha():
+                tokens.append((match, "IDENTIFIER"))
     return tokens
+
 
 def main():
     with open("input.txt", "r") as f:
