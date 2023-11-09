f46eaa8faa7eca61c15ae03f6aed7cd6650318eb
Frank9661 <98559684+Frank9661@users.noreply.github.com>
2023-11-08 18:06:58 -0800

    Working Branch
    Reads file correctly. Need to work on comments as it reads everything after //

--- main.py
+++ main.py
@@ -1,57 +1,35 @@
 import re
-#Imported re to handle regular expressions
 
-def tokenize_code(input_file):
-    # Defined regular expressions for different types of tokens
-    keyword = r'\b(if|else|return|while|for|int|float|char|void)\b'
-    separator = r'([{}();])'
-    operator = r'([=+\-*/<>]=?)'
-    identifier = r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'
-    integer = r'\b\d+\b'
-    comment = r'\/\/.*'
+KEYWORDS = ["if", "else", "return", "int", "float", "char", "void"]
+OPERATORS = [
+    "+", "-", "*", "/", "=", "<", ">", "&", "|", "^", "!", "~", "?", ":", "==", "!=", ">="
+]
+SEPARATORS = ["(", ")", "{", "}", "[", "]", ";", ","]
 
-    # Created a dictionary to map regular expressions to their corresponding token names
-    token_patterns = {
-        keyword: "keyword",
-        separator: "separator",
-        operator: "operator",
-        identifier: "identifier",
-        integer: "integer",
-        comment: "comment",
-    }
+def tokenize(code):
+    tokens = []
+    for match in re.finditer(r"(\b\w+\b)|(\W)", code):
+        match = match.group(0)
+        if match in KEYWORDS:
+            tokens.append((match, "KEYWORD"))
+        elif match in OPERATORS:
+            tokens.append((match, "OPERATOR"))
+        elif match in SEPARATORS:
+            tokens.append((match, "SEPARATOR"))
+        elif match.isdigit():
+            tokens.append((match, "INTEGER"))
+        elif match.isalpha():
+            tokens.append((match, "IDENTIFIER"))
+    return tokens
 
-    lexemes_and_tokens = []
+def main():
+    with open("input.txt", "r") as f:
+        code = f.read()
+    
+    tokens = tokenize(code)
 
-    with open(input_file, 'r') as file:
-        for line in file:
-            # Removes comments, hopefully
-            line = re.sub(comment, '', line)
-            line = line.strip()
-
-            if not line:
-                continue  # Skip empty lines
-
-            while line:
-                matched = False
-                for pattern, token_name in token_patterns.items():
-                    match = re.match(pattern, line)
-                    if match:
-                        lexeme = match.group(0)
-                        lexemes_and_tokens.append((lexeme, token_name))
-                        line = line[len(lexeme):]
-                        matched = True
-                        break
-
-                if not matched:
-                    print(f"Error: Unrecognized token at the beginning of the line: {line}")
-                    break
-
-    return lexemes_and_tokens
+    for token in tokens:
+        print(token)
 
 if __name__ == "__main__":
-    input_file = "input.txt"
-    lexemes_and_tokens = tokenize_code(input_file)
-
-    # Prints the result, hopefully
-    for lexeme, token in lexemes_and_tokens:
-        print(f'"{lexeme}" = {token}')
+    main()
