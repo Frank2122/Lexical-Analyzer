7db8cd10729af7f14dcda14b37c836fb465779e6
Frank9661 <98559684+Frank9661@users.noreply.github.com>
2023-11-10 18:20:44 -0800

    Updated comments again
    More comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re #Imported to read regular expressions
 
-#defined different KEYWORDS, OPERATORS, and SEPARATORS
+#Defined different KEYWORDS, OPERATORS, and SEPARATORS
 KEYWORDS = ["if", "else", "return", "int", "float", "char", "void"]
 OPERATORS = [
     "+", "-", "*", "/", "=", "<", ">", "&", "|", "^", "!", "~", "?", ":", "==", "!=", ">="
@@ -12,8 +12,7 @@ def tokenize(code):
     tokens = []
     lines = code.split('\n')
     for line in lines:
-        # Removes comment in each line. Needed to implement to handle phrases after "//"
-        line = line.split('//')[0]
+        line = line.split('//')[0] #Splits each line that use "//", and removes the comments present on that line following "//"
         for match in re.finditer(r"(\b\w+\b)|(\W)", line):
             match = match.group(0)
             if match in KEYWORDS:
@@ -30,7 +29,7 @@ def tokenize(code):
 
 
 def main():
-    with open("input.txt", "r") as f:
+    with open("input.txt", "r") as f: #Reads input.txt
         code = f.read()
     
     tokens = tokenize(code)
@@ -40,3 +39,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Much of our initial problem was met with handling comments. While we figured out reading and separating keywords,
+# operators, separators, integers, and identifiers, it began to label the comments as identifiers. We rectified this splitting
+# the line after "//" and removing the comments after it on the same line. That way we can ensure that code written 
+# after the comments can still be translated.
